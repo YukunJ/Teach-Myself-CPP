9a54a9fc4a03581833f31ef6d668722294fd0ce2
YukunJ <yukunj@andrew.cmu.edu>
2022-09-09 23:57:19 -0400

    Update: finish 1st version of SkipList implementation. Already find that the performance is not as good as expected

--- KV-store-skiplist/src/skiplist.h
+++ KV-store-skiplist/src/skiplist.h
@@ -1,11 +1,15 @@
 /**
  * skiplist.h
- * This is the implementation of a simple SkipList data structure using linked list
- * It supports three main APIs: SkipSearch, SkipInsert and SkipRemove
- * We use linked node of 4-directional links to connect the list and maintain the top-left header
-*/
+ * This is the implementation of a simple SkipList data structure using linked
+ * list It supports three main APIs: SkipSearch, SkipInsert and SkipRemove We
+ * use linked node of 4-directional links to connect the list and maintain the
+ * top-left header
+ */
 #ifndef KVSTORE_SKIPLIST_H
 #define KVSTORE_SKIPLIST_H
+
+#include <random>
+#include <vector>
 
 namespace kvstore {
 
@@ -15,135 +19,144 @@ namespace kvstore {
  * @tparam K key type
  * @tparam V value type
  */
-template<typename K, typename V>
+template <typename K, typename V>
 class SkipNode {
-public:
-    /**
-     * @brief create a new SkipNode object with the provided key and value
-     * @param key key
-     * @param value value
-     */
-    explicit SkipNode(K key, V value, bool is_sentinel = false): key_(key), value_(value), is_sentinel_(is_sentinel) {}
+ public:
+  /**
+   * @brief create a new SkipNode object with the provided key and value
+   * @param key key
+   * @param value value
+   */
+  explicit SkipNode(K key, V value, bool is_sentinel = false)
+      : key_(key), value_(value), is_sentinel_(is_sentinel) {}
 
-    /**
-     * @brief get the key this node is holding
-     * @return key
-     */
-    K GetKey() const { return key_; }
+  /**
+   * @brief get the key this node is holding
+   * @return key
+   */
+  K GetKey() const { return key_; }
 
-    /**
-     * @brief get the value this node is holding
-     * @return value
-     */
-    V GetValue() const { return value_; }
+  /**
+   * @brief get the value this node is holding
+   * @return value
+   */
+  V GetValue() const { return value_; }
 
-    /**
-     * @brief give access to the SkipNode before this node
-     * @return pointer to SkipNode
-     */
-    SkipNode *GetBefore() const { return before_; }
+  /**
+   * @brief overwrite the existing value by new value
+   * @param value the new value to be updated
+   */
+  void SetValue(V value) { value_ = value; }
 
-    /**
-     * @brief give access to the SkipNode after this node
-     * @return pointer to SkipNode
-     */
-    SkipNode *GetAfter() const { return after_; }
+  /**
+   * @brief give access to the SkipNode before this node
+   * @return pointer to SkipNode
+   */
+  SkipNode *GetBefore() const { return before_; }
 
-    /**
-     * @brief give access to the SkipNode below this node
-     * @return pointer to SkipNode
-     */
-    SkipNode *GetBelow() const { return below_; }
+  /**
+   * @brief give access to the SkipNode after this node
+   * @return pointer to SkipNode
+   */
+  SkipNode *GetAfter() const { return after_; }
 
-    /**
-     * @brief give access to the SkipNode above this node
-     * @return pointer to SkipNode
-     */
-    SkipNode *GetAbove() const { return above_; }
+  /**
+   * @brief give access to the SkipNode below this node
+   * @return pointer to SkipNode
+   */
+  SkipNode *GetBelow() const { return below_; }
 
-    /**
-     * @brief if this node's value is a sentinel node
-     * @return true if positive infinity, false otherwise
-     */
-    bool IsSentinel() const { return is_sentinel_; }
+  /**
+   * @brief give access to the SkipNode above this node
+   * @return pointer to SkipNode
+   */
+  SkipNode *GetAbove() const { return above_; }
 
-    /**
-     * @brief set the before node
-     * @param node the pointer to SkipNode as the before node
-     */
-    void SetBefore(SkipNode *node) { before_ = node; }
+  /**
+   * @brief if this node's value is a sentinel node
+   * @return true if positive infinity, false otherwise
+   */
+  bool IsSentinel() const { return is_sentinel_; }
 
-    /**
-     * @brief set the after node
-     * @param node the pointer to SkipNode as the after node
-     */
-    void SetAfter(SkipNode *node) { after_ = node; }
+  /**
+   * @brief set the before node
+   * @param node the pointer to SkipNode as the before node
+   */
+  void SetBefore(SkipNode *node) { before_ = node; }
 
-    /**
-     * @brief set the below node
-     * @param node the pointer to SkipNode as the below node
-     */
-    void SetBelow(SkipNode *node) { below_ = node; }
+  /**
+   * @brief set the after node
+   * @param node the pointer to SkipNode as the after node
+   */
+  void SetAfter(SkipNode *node) { after_ = node; }
 
-    /**
-     * @brief set the above node
-     * @param node the pointer to SkipNode as the above node
-     */
-    void SetAbove(SkipNode *node) { above_ = node; }
+  /**
+   * @brief set the below node
+   * @param node the pointer to SkipNode as the below node
+   */
+  void SetBelow(SkipNode *node) { below_ = node; }
 
-    /**
-     * @brief set this node's value to represent positive infinity
-     */
-    void SetSentinel() { is_sentinel_ = true; }
+  /**
+   * @brief set the above node
+   * @param node the pointer to SkipNode as the above node
+   */
+  void SetAbove(SkipNode *node) { above_ = node; }
 
-    /**
-     * @brief starting from this node, search the node with
-     *        biggest key that's smaller or equal to the one provided
-     * @param key the key for search
-     * @return pointer to SkipNode
-     */
-    SkipNode *SkipSearch(K key) const {
-        auto curr = this;
-        while (true) {
-            while (curr->ShouldSkipRight(key)) {
-                curr = curr->GetAfter();
-            }
-            if (curr->GetBelow() != nullptr) {
-                curr = curr->GetBelow();
-            } else {
-                break;
-            }
-        }
-        return const_cast<SkipNode*>(curr);
+  /**
+   * @brief set this node's value to represent positive infinity
+   */
+  void SetSentinel() { is_sentinel_ = true; }
+
+  /**
+   * @brief starting from this node, search the node with
+   *        biggest key that's smaller or equal to the one provided
+   * @param key the key for search
+   * @return pair of pointer to SkipNode and path of rightmost nodes traversed
+   * on each level
+   */
+  std::pair<SkipNode *, std::vector<SkipNode *>> SkipSearch(K key) const {
+    std::vector<SkipNode *> path;
+    auto curr = this;
+    while (true) {
+      while (curr->ShouldSkipRight(key)) {
+        curr = curr->GetAfter();
+      }
+      path.push_back(const_cast<SkipNode *>(curr));
+      if (curr->GetBelow() != nullptr) {
+        curr = curr->GetBelow();
+      } else {
+        break;
+      }
     }
+    return {const_cast<SkipNode *>(curr), path};
+  }
 
-private:
-    /**
-     * @brief compare with the key, if should proceed going right
-     * @param key the provided key
-     * @return true if should proceed, false otherwise
-     */
-    bool ShouldSkipRight(K key) const {
-        if (after_ == nullptr || after_->IsSentinel()) {
-            return false;
-        }
-        return after_->GetKey() <= key;
+ private:
+  /**
+   * @brief compare with the key, if should proceed going right
+   * @param key the provided key
+   * @return true if should proceed, false otherwise
+   */
+  bool ShouldSkipRight(K key) const {
+    if (after_ == nullptr || after_->IsSentinel()) {
+      return false;
     }
-
-    /** if the node is a sentinel node */
-    bool is_sentinel_ = false;
-    /** the pointer to the SkipNode before */
-    SkipNode *before_ = nullptr;
-    /** the pointer to the SkipNode after */
-    SkipNode *after_ = nullptr;
-    /** the pointer to the SkipNode below */
-    SkipNode *below_ = nullptr;
-    /** the pointer to the SkipNode above */
-    SkipNode *above_ = nullptr;
-    /** key for this node */
-    K key_;
-    /** real value for this node (dominated by neg_inf or inf) */
-    V value_;
+    return after_->GetKey() <= key;
+  }
+  /** if the node is a sentinel node */
+  bool is_sentinel_ = false;
+  /** the pointer to the SkipNode before */
+  SkipNode *before_ = nullptr;
+  /** the pointer to the SkipNode after */
+  SkipNode *after_ = nullptr;
+  /** the pointer to the SkipNode below */
+  SkipNode *below_ = nullptr;
+  /** the pointer to the SkipNode above */
+  SkipNode *above_ = nullptr;
+  /** key for this node */
+  K key_;
+  /** real value for this node (dominated by neg_inf or inf) */
+  V value_;
 };
 
 /**
@@ -152,56 +165,175 @@ private:
  * @tparam K key type
  * @tparam V value type
  */
-template<typename K, typename V>
+template <typename K, typename V>
 class SkipList {
-public:
-    /**
-     * @brief create a new SkipList object
-     * @param max_height the maximum height allowed to grow
-     */
-    explicit SkipList(int max_height = 10);
+ public:
+  /**
+   * @brief create a new SkipList object
+   * @param max_height the maximum height allowed to grow
+   */
+  explicit SkipList(int max_height = 20) : max_height_(max_height) {
+    // create the first layer of sentinel nodes
+    head = new SkipNode<K, V>(K{}, V{}, true);
+    auto tail = new SkipNode<K, V>(K{}, V{}, true);
+    head->SetAfter(tail);
+    tail->SetBefore(head);
+    curr_height_ = 1;
+  }
 
-    /**
-     * @brief dtor to release all the SkipNodes in the SkipList
-     */
-    ~SkipList();
+  /**
+   * @brief dtor to release all the SkipNodes in the SkipList
+   */
+  ~SkipList() {
+    // level-by-level dynamic memory release
+    auto curr = head;
+    while (curr) {
+      auto temp = curr->GetBelow();
+      ReleaseLevel(curr);
+      curr = temp;
+    }
+  }
 
-    /**
-     * @brief search in the SkipList with given key
-     * @param key key for search
-     * @return the SkipNode with largest key that's smaller or equal to key
-     */
-    SkipNode<K, V> *SkipSearch(K key);
+  /**
+   * @brief search in the SkipList with given key
+   * @param key key for search
+   * @return the SkipNode with largest key that's smaller or equal to key
+   */
+  SkipNode<K, V> *SkipSearch(K key) { return head->SkipSearch(key).first; }
 
-    /**
-     * @brief insert into the SkipList of a key-value pair
-     * @param key the key
-     * @param value the value
-     * @return true if insertion is successful, false otherwise
-     */
-    bool SkipInsert(K key, V value);
+  /**
+   * @brief insert into the SkipList of a key-value pair
+   * @param key the key
+   * @param value the value
+   * @return true if insertion is new, false if replace old key-value pair
+   */
+  bool SkipInsert(K key, V value) {
+    auto search_pair = head->SkipSearch(key);
+    auto match = search_pair.first;
+    auto path = search_pair.second;
+    if (match->GetKey() == key && !match->IsSentinel()) {
+      // 1. already exist, replace the whole column's value by new value
+      while (match) {
+        auto temp = match->GetAbove();
+        match->SetValue(value);
+        match = temp;
+      }
+      return false;  // indicate a new value replacement for whole column
+    } else {
+      // 2. a new key-value to be inserted
+      int extend_height = rand() % max_height_ + 1;
+      if (extend_height > curr_height_) {
+        // build new sentinel layer until enough
+        int diff = extend_height - curr_height_;
+        while (diff--) {
+          BuildExtraLayer();
+        }
+        // re-fetch the search path (not optimal)
+        auto research_pair = head->SkipSearch(key);
+        match = research_pair.first;
+        path = research_pair.second;
+      }
+      // build the new node all the way up, after each path[i]
+      SkipNode<K, V> *last = nullptr;
+      for (int i = path.size() - 1; i >= 0; i--) {
+        auto pre_node = path[i];
+        auto after_node = pre_node->GetAfter();
+        auto new_node = new SkipNode<K, V>(key, value);
+        pre_node->SetAfter(new_node);
+        after_node->SetBefore(new_node);
+        new_node->SetBefore(pre_node);
+        new_node->SetAfter(after_node);
+        new_node->SetBelow(last);
+        if (last) {
+          last->SetAbove(new_node);
+        }
+        last = new_node;
+      }
+      curr_size_++;
+      return true;
+    }
+  }
 
-    /**
-     * @brief remove a key from the SkipList
-     * @param key the key
-     * @return true if removal is successful, false otherwise
-     */
-    bool SkipRemove(K key);
+  /**
+   * @brief remove a key from the SkipList
+   * @param key the key
+   * @return true if removal is successful, false otherwise
+   */
+  bool SkipRemove(K key) {
+    auto match = SkipSearch(key);
+    if (match->GetKey() != key || match->IsSentinel()) {
+      // not exist in SkipList or is sentinel node
+      return false;
+    } else {
+      // remove the whole column
+      auto curr = match;
+      while (curr) {
+        auto temp = curr->GetAbove();
+        auto prev = curr->GetBefore();
+        auto next = curr->GetAfter();
+        prev->SetAfter(next);
+        next->SetBefore(prev);
+        delete curr;
+        curr = temp;
+      }
+      return true;
+    }
+  }
 
-private:
+  /**
+   * @brief return how many key-value pair are present in the SkipList
+   * @return the number of key-value pairs in the SkipList
+   */
+  std::size_t GetSize() { return curr_size_; }
 
-    /** the maximum height we allow the SkipList to grow */
-    int max_height_;
+ private:
+  /**
+   * @brief build a new layer on top of current head with two sentinel nodes
+   */
+  void BuildExtraLayer() {
+    auto new_head = new SkipNode<K, V>(K{}, V{}, true);
+    auto new_tail = new SkipNode<K, V>(K{}, V{}, true);
+    head->SetAbove(new_head);
+    new_head->SetBelow(head);
+    new_head->SetAfter(new_tail);
+    new_tail->SetBefore(new_head);
 
-    /** the current level of the SkipList */
-    int curr_height_ = 0;
+    // find the old top-level's tail
+    auto curr = head;
+    while (curr->GetAfter() != nullptr) {
+      curr = curr->GetAfter();
+    }
+    new_tail->SetBelow(curr);
+    curr->SetAbove(new_tail);
 
-    /** how many key-value pairs are contained in the SkipList */
-    std::size_t curr_count_ = 0;
+    curr_height_++;
+    head = new_head;
+  }
 
-    /** the top-left sentinel SkipNode in the SkipList */
-    SkipNode<K,V> *head = nullptr;
+  /**
+   * @brief release the whole row of SkipNodes starting from the given one
+   * @param node the head of a row of SkipNodes
+   */
+  void ReleaseLevel(SkipNode<K, V> *node) {
+    while (node) {
+      auto temp = node->GetAfter();
+      delete node;
+      node = temp;
+    }
+  }
+
+  /** the maximum height we allow the SkipList to grow */
+  int max_height_;
+
+  /** the current level of the SkipList */
+  int curr_height_ = 0;
+
+  /** how many key-value pairs are contained in the SkipList */
+  std::size_t curr_size_ = 0;
+
+  /** the top-left sentinel SkipNode in the SkipList */
+  SkipNode<K, V> *head = nullptr;
 };
-} // namespace kvstore
+}  // namespace kvstore
 
 #endif
--- KV-store-skiplist/test/skiplist_test.cpp
+++ KV-store-skiplist/test/skiplist_test.cpp
@@ -1,198 +1,223 @@
-#include <gtest/gtest.h>
 #include "../src/skiplist.h"
+
+#include <gtest/gtest.h>
 
 namespace kvstore {
 TEST(SkipNodeTest, SkipNodeLinkage) {
-    // test if the SkipNode's linkage is functioning correctly
+  // test if the SkipNode's linkage is functioning correctly
 
-    /*
-     create a linkage like:
-          (6, 20)
-             |
-    -oo - (2,4) - +00
-     */
-    auto handle_start = new SkipNode<int, int>(0, 0, true);
-    auto handle_end = new SkipNode<int, int>(0, 0, true);
-    auto center = new SkipNode<int, int>(2, 4);
-    handle_start->SetAfter(center);
-    handle_end->SetBefore(center);
-    center->SetBefore(handle_start);
-    center->SetAfter(handle_end);
-    center->SetAbove(new SkipNode<int, int>(6, 20));
-    center->GetAbove()->SetBelow(center);
+  /*
+   create a linkage like:
+        (6, 20)
+           |
+  -oo - (2,4) - +00
+   */
+  auto handle_start = new SkipNode<int, int>(0, 0, true);
+  auto handle_end = new SkipNode<int, int>(0, 0, true);
+  auto center = new SkipNode<int, int>(2, 4);
+  handle_start->SetAfter(center);
+  handle_end->SetBefore(center);
+  center->SetBefore(handle_start);
+  center->SetAfter(handle_end);
+  center->SetAbove(new SkipNode<int, int>(6, 20));
+  center->GetAbove()->SetBelow(center);
 
-    EXPECT_EQ(center->GetKey(), 2);
-    EXPECT_EQ(center->GetValue(), 4);
-    EXPECT_EQ(center->GetAbove()->GetKey(), 6);
-    EXPECT_EQ(center->GetBelow(), nullptr);
+  EXPECT_EQ(center->GetKey(), 2);
+  EXPECT_EQ(center->GetValue(), 4);
+  EXPECT_EQ(center->GetAbove()->GetKey(), 6);
+  EXPECT_EQ(center->GetBelow(), nullptr);
 
-    // clean up
-    free(center->GetBefore());
-    free(center->GetAfter());
-    free(center->GetAbove());
-    free(center);
+  // clean up
+  free(center->GetBefore());
+  free(center->GetAfter());
+  free(center->GetAbove());
+  free(center);
 }
 
 TEST(SkipNodeTest, SkipNodeSkipSearch) {
-    // test if the SkipNode's SkipSearch is working correctly
-    // find the biggest node of key that's smaller or equal to given key
+  // test if the SkipNode's SkipSearch is working correctly
+  // find the biggest node of key that's smaller or equal to given key
 
-    // create such a topology of SkipNode linkages
-    /*
-      -oo <-> 2 <-------------------------> 9 <-> +oo
-       |      |                             |       |
-      -oo <-> 2 <---------5 <-> 7---------> 9 <-> +oo
-       |      |           |     |           |       |
-      -oo <-> 2 <-> 4 <-> 5 <-> 7---------> 9 <-> +oo
-    */
-    auto head_1 = new SkipNode<int, int>(0, 0, true);
-    auto head_2 = new SkipNode<int, int>(0, 0, true);
-    auto head_3 = new SkipNode<int, int>(0, 0, true);
+  // create such a topology of SkipNode linkages
+  /*
+    -oo <-> 2 <-------------------------> 9 <-> +oo
+     |      |                             |       |
+    -oo <-> 2 <---------5 <-> 7---------> 9 <-> +oo
+     |      |           |     |           |       |
+    -oo <-> 2 <-> 4 <-> 5 <-> 7---------> 9 <-> +oo
+  */
+  auto head_1 = new SkipNode<int, int>(0, 0, true);
+  auto head_2 = new SkipNode<int, int>(0, 0, true);
+  auto head_3 = new SkipNode<int, int>(0, 0, true);
 
-    auto tail_1 = new SkipNode<int, int>(0, 0, true);
-    auto tail_2 = new SkipNode<int, int>(0, 0, true);
-    auto tail_3 = new SkipNode<int, int>(0, 0, true);
+  auto tail_1 = new SkipNode<int, int>(0, 0, true);
+  auto tail_2 = new SkipNode<int, int>(0, 0, true);
+  auto tail_3 = new SkipNode<int, int>(0, 0, true);
 
-    auto node_2_1 = new SkipNode<int, int>(2, 0);
-    auto node_2_2 = new SkipNode<int, int>(2, 0);
-    auto node_2_3 = new SkipNode<int, int>(2, 0);
+  auto node_2_1 = new SkipNode<int, int>(2, 0);
+  auto node_2_2 = new SkipNode<int, int>(2, 0);
+  auto node_2_3 = new SkipNode<int, int>(2, 0);
 
-    auto node_9_1 = new SkipNode<int, int>(9, 0);
-    auto node_9_2 = new SkipNode<int, int>(9, 0);
-    auto node_9_3 = new SkipNode<int, int>(9, 0);
+  auto node_9_1 = new SkipNode<int, int>(9, 0);
+  auto node_9_2 = new SkipNode<int, int>(9, 0);
+  auto node_9_3 = new SkipNode<int, int>(9, 0);
 
-    auto node_5_2 = new SkipNode<int, int>(5, 0);
-    auto node_5_3 = new SkipNode<int, int>(5, 0);
+  auto node_5_2 = new SkipNode<int, int>(5, 0);
+  auto node_5_3 = new SkipNode<int, int>(5, 0);
 
-    auto node_7_2 = new SkipNode<int, int>(7, 0);
-    auto node_7_3 = new SkipNode<int, int>(7, 0);
+  auto node_7_2 = new SkipNode<int, int>(7, 0);
+  auto node_7_3 = new SkipNode<int, int>(7, 0);
 
-    auto node_4_3 = new SkipNode<int, int>(4, 0);
+  auto node_4_3 = new SkipNode<int, int>(4, 0);
 
-    // we only link "after" and "below" linkage here for convenience
-    // construct first layer
-    head_1->SetAfter(node_2_1);
-    node_2_1->SetAfter(node_9_1);
-    node_9_1->SetAfter(tail_1);
+  // we only link "after" and "below" linkage here for convenience
+  // construct first layer
+  head_1->SetAfter(node_2_1);
+  node_2_1->SetAfter(node_9_1);
+  node_9_1->SetAfter(tail_1);
 
-    // construct second layer
-    head_2->SetAfter(node_2_2);
-    node_2_2->SetAfter(node_5_2);
-    node_5_2->SetAfter(node_7_2);
-    node_7_2->SetAfter(node_9_2);
-    node_9_2->SetAfter(tail_2);
+  // construct second layer
+  head_2->SetAfter(node_2_2);
+  node_2_2->SetAfter(node_5_2);
+  node_5_2->SetAfter(node_7_2);
+  node_7_2->SetAfter(node_9_2);
+  node_9_2->SetAfter(tail_2);
 
-    // construct third layer
-    head_3->SetAfter(node_2_3);
-    node_2_3->SetAfter(node_4_3);
-    node_4_3->SetAfter(node_5_3);
-    node_5_3->SetAfter(node_7_3);
-    node_7_3->SetAfter(node_9_3);
-    node_9_3->SetAfter(tail_3);
+  // construct third layer
+  head_3->SetAfter(node_2_3);
+  node_2_3->SetAfter(node_4_3);
+  node_4_3->SetAfter(node_5_3);
+  node_5_3->SetAfter(node_7_3);
+  node_7_3->SetAfter(node_9_3);
+  node_9_3->SetAfter(tail_3);
 
-    // link first and second layer
-    head_1->SetBelow(head_2);
-    node_2_1->SetBelow(node_2_2);
-    node_9_1->SetBelow(node_9_2);
-    tail_1->SetBelow(tail_2);
+  // link first and second layer
+  head_1->SetBelow(head_2);
+  node_2_1->SetBelow(node_2_2);
+  node_9_1->SetBelow(node_9_2);
+  tail_1->SetBelow(tail_2);
 
-    // link second and third layer
-    head_2->SetBelow(head_3);
-    node_2_2->SetBelow(node_2_3);
-    node_5_2->SetBelow(node_5_3);
-    node_7_2->SetBelow(node_7_3);
-    node_9_2->SetBelow(node_9_3);
-    tail_2->SetBelow(tail_3);
+  // link second and third layer
+  head_2->SetBelow(head_3);
+  node_2_2->SetBelow(node_2_3);
+  node_5_2->SetBelow(node_5_3);
+  node_7_2->SetBelow(node_7_3);
+  node_9_2->SetBelow(node_9_3);
+  tail_2->SetBelow(tail_3);
 
-    auto ans1 = head_1->SkipSearch(6);
-    EXPECT_EQ(ans1->GetKey(), 5);
+  auto ans1 = head_1->SkipSearch(6).first;
+  EXPECT_EQ(ans1->GetKey(), 5);
 
-    auto ans2 = head_1->SkipSearch(5);
-    EXPECT_EQ(ans2->GetKey(), 5);
+  auto ans2 = head_1->SkipSearch(5).first;
+  EXPECT_EQ(ans2->GetKey(), 5);
 
-    auto ans3 = head_1->SkipSearch(10);
-    EXPECT_EQ(ans3->GetKey(), 9);
+  auto ans3 = head_1->SkipSearch(10).first;
+  EXPECT_EQ(ans3->GetKey(), 9);
 
-    auto ans4 = head_1->SkipSearch(3);
-    EXPECT_EQ(ans4->GetKey(), 2);
+  auto ans4 = head_1->SkipSearch(3).first;
+  EXPECT_EQ(ans4->GetKey(), 2);
 
-    auto ans5 = head_1->SkipSearch(1);
-    EXPECT_EQ(ans5->IsSentinel(), true);
+  auto ans5 = head_1->SkipSearch(1).first;
+  EXPECT_EQ(ans5->IsSentinel(), true);
 
-    // clean up
-    delete head_1;
-    delete head_2;
-    delete head_3;
-    delete tail_1;
-    delete tail_2;
-    delete tail_3;
-    delete node_2_1;
-    delete node_2_2;
-    delete node_2_3;
-    delete node_9_1;
-    delete node_9_2;
-    delete node_9_3;
-    delete node_5_2;
-    delete node_5_3;
-    delete node_7_2;
-    delete node_7_3;
-    delete node_4_3;
+  // clean up
+  delete head_1;
+  delete head_2;
+  delete head_3;
+  delete tail_1;
+  delete tail_2;
+  delete tail_3;
+  delete node_2_1;
+  delete node_2_2;
+  delete node_2_3;
+  delete node_9_1;
+  delete node_9_2;
+  delete node_9_3;
+  delete node_5_2;
+  delete node_5_3;
+  delete node_7_2;
+  delete node_7_3;
+  delete node_4_3;
 }
 
 TEST(SkipListTest, SkipListInsertSearchTest) {
-    // test if the SkipList could handle insert and search correctly
-    SkipList<int, int> skip;
-    skip.SkipInsert(1, 12);
-    skip.SkipInsert(4, 13);
-    skip.SkipInsert(5, 53);
-    skip.SkipInsert(-2, 2);
+  // test if the SkipList could handle insert and search correctly
+  SkipList<int, int> skip;
+  skip.SkipInsert(1, 12);
+  skip.SkipInsert(4, 13);
+  skip.SkipInsert(5, 53);
+  skip.SkipInsert(-2, 2);
 
-    auto res1 = skip.SkipSearch(4);
-    EXPECT_EQ(res1->GetKey(), 4);
-    EXPECT_EQ(res1->GetValue(), 13);
+  auto res1 = skip.SkipSearch(4);
+  EXPECT_EQ(res1->GetKey(), 4);
+  EXPECT_EQ(res1->GetValue(), 13);
 
-    auto res2 = skip.SkipSearch(0);
-    EXPECT_EQ(res2->GetKey(), -2);
-    EXPECT_EQ(res2->GetValue(), 2);
+  auto res2 = skip.SkipSearch(0);
+  EXPECT_EQ(res2->GetKey(), -2);
+  EXPECT_EQ(res2->GetValue(), 2);
 
-    auto res3 = skip.SkipSearch(6);
-    EXPECT_EQ(res3->GetKey(), 5);
-    EXPECT_EQ(res3->GetValue(), 53);
+  auto res3 = skip.SkipSearch(6);
+  EXPECT_EQ(res3->GetKey(), 5);
+  EXPECT_EQ(res3->GetValue(), 53);
+
+  // rely on dtor to clean up
+}
+
+TEST(SkipListTest, SkipListHeavyInsertTest) {
+  SkipList<int, int> skip;
+  int test_size = 10000;
+  for (int i = 0; i < test_size; i++) {
+    skip.SkipInsert(i, i);
+  }
+
+  EXPECT_EQ(skip.GetSize(), test_size);
+
+  for (int i = 0; i < test_size; i++) {
+    EXPECT_EQ(skip.SkipSearch(i)->GetValue(), i);
+  }
+
+  // rely on dtor to clean up
 }
 
 TEST(SkipListTest, SkipListSameKeyInsertTest) {
-    // test if the SkipList would replace old value when a key is repeated inserted
-    SkipList<int, int> skip;
-    skip.SkipInsert(1, 12);
-    skip.SkipInsert(4, 13);
-    skip.SkipInsert(5, 53);
-    skip.SkipInsert(-2, 2);
+  // test if the SkipList would replace old value when a key is repeated
+  // inserted
+  SkipList<int, int> skip;
+  skip.SkipInsert(1, 12);
+  skip.SkipInsert(4, 13);
+  skip.SkipInsert(5, 53);
+  skip.SkipInsert(-2, 2);
 
-    skip.SkipInsert(-2, 4);
-    skip.SkipInsert(-2, 5);
-    auto res = skip.SkipSearch(-2);
-    EXPECT_EQ(res->GetKey(), -2);
-    EXPECT_EQ(res->GetValue(), 5);
+  skip.SkipInsert(-2, 4);
+  skip.SkipInsert(-2, 5);
+  auto res = skip.SkipSearch(-2);
+  EXPECT_EQ(res->GetKey(), -2);
+  EXPECT_EQ(res->GetValue(), 5);
 
-    skip.SkipInsert(4, 4);
-    skip.SkipInsert(4, 9);
-    auto res2 = skip.SkipSearch(4);
-    EXPECT_EQ(res2->GetKey(), 4);
-    EXPECT_EQ(res2->GetValue(), 9);
+  skip.SkipInsert(4, 4);
+  skip.SkipInsert(4, 9);
+  auto res2 = skip.SkipSearch(4);
+  EXPECT_EQ(res2->GetKey(), 4);
+  EXPECT_EQ(res2->GetValue(), 9);
+
+  // rely on dtor to clean up
 }
 
 TEST(SkipListTest, SkipListRemovalTest) {
-    // test if the SkipList could handle delete non-exist key, repeat delete the same key, etc.
-    SkipList<int, int> skip;
-    skip.SkipInsert(1, 12);
-    skip.SkipInsert(4, 13);
-    skip.SkipInsert(5, 53);
-    skip.SkipInsert(-2, 2);
+  // test if the SkipList could handle delete non-exist key, repeat delete the
+  // same key, etc.
+  SkipList<int, int> skip;
+  skip.SkipInsert(1, 12);
+  skip.SkipInsert(4, 13);
+  skip.SkipInsert(5, 53);
+  skip.SkipInsert(-2, 2);
 
-    EXPECT_EQ(skip.SkipRemove(6), false);
-    EXPECT_EQ(skip.SkipRemove(5), true);
-    EXPECT_EQ(skip.SkipRemove(5), false);
-    EXPECT_NE(skip.SkipSearch(5)->GetKey(), 5);
+  EXPECT_EQ(skip.SkipRemove(6), false);
+  EXPECT_EQ(skip.SkipRemove(5), true);
+  EXPECT_EQ(skip.SkipRemove(5), false);
+  EXPECT_NE(skip.SkipSearch(5)->GetKey(), 5);
+
+  // rely on dtor to clean up
 }
 
-} // namespace kvstore
+}  // namespace kvstore
