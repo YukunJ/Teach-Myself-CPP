09842be21f62c796ded550667d4b8365ff03949e
YukunJ <yukunj.cs@gmail.com>
2026-09-03 00:57:06 +0000

    Hazard pointer bug fix w.r.t memory ordering

--- ABA-Memory-Reclamation-Problem/src/lockfree_stack_hazard_pointer.cpp
+++ ABA-Memory-Reclamation-Problem/src/lockfree_stack_hazard_pointer.cpp
@@ -5,22 +5,13 @@
 template<typename T>
 void LockfreeStack<T>::push(const T& value, size_t thread_id) {
     Node *new_node = new Node(value);
-    Node *old_head;
-    while (true) {
-        old_head = head_.load(std::memory_order_acquire);
-        hazard_pointers_[thread_id].ptr_.store(old_head, std::memory_order_seq_cst);
-        if (old_head != head_.load(std::memory_order_seq_cst)) {
-            // ensure between we load the head and publish the hazard pointer, the head has not changed
-            continue;
-        }
-        
+    Node *old_head = head_.load(std::memory_order_acquire);
+    // it's not needed to set hazard pointer here since we are not going to read/refernce the old head node
+    // even if another thread pop and reallocate a head using the same address of the old head
+    // we can still just take the new head and link to it
+    do {
         new_node->next_ = old_head;
-        if (head_.compare_exchange_weak(old_head, new_node, std::memory_order_release, std::memory_order_relaxed)) {
-            hazard_pointers_[thread_id].ptr_.store(nullptr, std::memory_order_release);
-            break;
-        }
-        // don't bother to clean up the hazard pointer here since we will overwrite it in the next iteration anyway
-    }
+    } while (!head_.compare_exchange_weak(old_head, new_node, std::memory_order_release, std::memory_order_relaxed));
 }
 
 template<typename T>
@@ -39,9 +30,12 @@ std::optional<T> LockfreeStack<T>::pop(size_t thread_id) {
         }
 
         Node *next_node = old_head->next_;
-        if (head_.compare_exchange_weak(old_head, next_node, std::memory_order_acquire, std::memory_order_relaxed)) {
+        // important to use seq_cst here for the success publish case
+        // to let the unlink participate in the global ordering of
+        // hazard pointer publication and re-verify 
+        if (head_.compare_exchange_weak(old_head, next_node, std::memory_order_seq_cst, std::memory_order_relaxed)) {
             T to_ret = std::move(old_head->value_);
-            hazard_pointers_[thread_id].ptr_.store(nullptr, std::memory_order_release);
+            hazard_pointers_[thread_id].ptr_.store(nullptr, std::memory_order_seq_cst);
             retire_node(old_head);
             return to_ret;
         }
